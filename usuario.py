1a15512adc88466aef877a438b1fe1dc245d1731
JuaniTorti <nachi.tortjuani@gmail.com>
2023-10-18 15:56:46 -0300

    corrijo los __str__()

--- usuario.py
+++ usuario.py
@@ -1,7 +1,8 @@
 from cod_generator import generar
 
+
 class Usuario:
-    
+
     def __init__(self, nombre: str, apellido: str, email: str, contra: str) -> None:
         self.__nombre = nombre
         self.__apellido = apellido
@@ -9,14 +10,10 @@ class Usuario:
         self.__contra = contra
 
     def __str__(self) -> str:
-        print("Nombre: ", self.__nombre)
-        print("Apellido: ", self.__apellido)
-        print("Email: ", self.__email)
-        print("Contraseña: ", self.__contra)
+        return f"""\nNombre: {self.__nombre} \nApellido: {self.__apellido} \nEmail: {self.__email} \nContraseña: {self.__contra}"""
 
     def validar_credenciales(self, email: str, contra: str):
         pass
-
 
 
 class Curso:
@@ -26,35 +23,26 @@ class Curso:
         self.__clave = ""
 
     def __str__(self) -> str:
-        print("Nombre: ", self.__nombre)
-        print("Clave de matriculacion: ", self.__clave)
+        return f"""Nombre: {self.__nombre} \nClave de matriculacion: {self.__clave}"""
 
     def __generar_contraseña(self):
         self.__clave = generar()
 
 
-
-
-
 class Estudiante(Usuario):
-    
+
     def __init__(self, nombre: str, apellido: str, email: str, contra: str, legajo: int, anio_inscripcion: int) -> None:
         super().__init__(nombre, apellido, email, contra)
         self.__legajo = legajo
         self.__anio_inscripcion = anio_inscripcion
-        self.__mis_cursos = [] # aca se almacenaran los cursos que cursa
+        self.__mis_cursos = []  # aca se almacenaran los cursos que cursa
 
     def __str__(self) -> str:
-        print(super().__str__()) 
-        print("Legajo: ", self.__legajo)
-        print("Año de inscripcion: ", self.__anio_inscripcion)
-        print("Cursando: ", self.__mis_cursos)
+
+        return f"""{super().__str__()} \nLegajo: {self.__legajo} \nAño de inscripcion: {self.__anio_inscripcion} \nCursando: {self.__mis_cursos}"""
 
     def matricular_en_curso(self, curso: Curso):
         pass
-
-
-
 
 
 class Profesor(Usuario):
@@ -63,17 +51,10 @@ class Profesor(Usuario):
         super().__init__(nombre, apellido, email, contra)
         self.__titulo = titulo
         self.__anio_egreso = anio_egreso
-        self.__mis_cursos = [] # aca se almacenaran los cursos que da
+        self.__mis_cursos = []  # aca se almacenaran los cursos que da
 
     def __str__(self) -> str:
-        print(super().__str__())
-        print("Titulo: ", self.__titulo)
-        print("Año de egreso: ", self.__anio_egreso)
-        print("Cursos que dicta: ", self.__mis_cursos)
+        return f"""{super().__str__()} \nTitulo: {self.__titulo} \nAño de egreso: {self.__anio_egreso} \nCurso que dicta: {self.__mis_cursos}"""
 
     def dictar_curso(self, curso: Curso):
         pass
-
-
-
-
